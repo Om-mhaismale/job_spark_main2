ad6c2175424fa65c36c23da7637d3a9668b4acba
om mhaismale <ommhaismale13@gmail.com>
2025-09-14 21:40:07 +0530

    same change

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,9 +2,10 @@ export default function Form() {
     return (
         <>
             <button>add resume</button>
-            <button onClick={() => window.open('https://jobspark-resumebuilt.vercel.app/')}>
-                Re-write Resume
+            <button onClick={() => window.location.href = 'https://jobspark-resumebuilt.vercel.app/'}>
+                Re-Built Resume
             </button>
+
             <button>Find Jobs</button>
         </>
     )
