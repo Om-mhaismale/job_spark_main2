ebd0a7441e9af6e1ee10501dbe96de988ce8b6b9
om mhaismale <ommhaismale13@gmail.com>
2025-09-14 21:33:09 +0530

    removed target blank

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,7 @@ export default function Form() {
     return (
         <>
             <button>add resume</button>
-            <button onClick={() => window.open('https://jobspark-resumebuilt.vercel.app/', '_blank')}>
+            <button onClick={() => window.open('https://jobspark-resumebuilt.vercel.app/')}>
                 Re-write Resume
             </button>
             <button>Find Jobs</button>
