b10b7b2bb9aa19675c11fb9a3df621b1e96deca7
Parth Rana <parth.rana@somaiya.edu>
2025-09-21 19:16:40 +0530

    made changes to theme (linkedin blue)

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,8 +1,12 @@
+import '../styles/content.css'
 export default function Content() {
     return(
         <>
+
+        <div className="content-main">
         <h2>Your Gateway to Exciting Careers</h2>
         <p>Discover your dream job with Job Spark, the ultimate platform connecting talent with opportunity.<br></br>
      Explore thousands of job listings, get personalized recommendations, and take the next step in your career journey today!</p>
+     </div>
         </>
     )}
